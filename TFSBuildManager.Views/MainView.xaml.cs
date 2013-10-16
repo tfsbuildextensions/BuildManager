69cfcabf439c09066a0d70b4eaaa8e47182683ca
mikeFourie_cp <SND\mikeFourie_cp>
2013-10-16 12:57:34 -0700

    TFSBM - allow deletion of build definitions using del key

--- TFSBuildManager.Views/MainView.xaml.cs
+++ TFSBuildManager.Views/MainView.xaml.cs
@@ -330,5 +330,13 @@ namespace TfsBuildManager.Views
                 this.DisplayError(ex);
             }
         }
+
+        private void BuildDefinitionList_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Delete)
+            {
+                this.viewmodel.OnDelete();
+            }
+        }
     }
 }
